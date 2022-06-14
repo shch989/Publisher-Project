083880a788f98efbac030571f97a9dc78729cbcf
shch989 <tankpop11@gmail.com>
2022-06-15 01:23:45 +0900

    사진 업로드

--- js/main.js
+++ js/main.js
@@ -16,11 +16,6 @@ fadeEls.forEach(function (fadeEl, index) {
 /**
  * 슬라이드 요소 관리
  */
-new Swiper('.notice-line .swiper-container', {
-  direction: 'vertical', // 수직 슬라이드
-  autoplay: true, // 자동 재생 여부
-  loop: true // 반복 재생 여부
-})
 new Swiper('.promotion .swiper-container', {
   // direction: 'horizontal', // 수평 슬라이드
   autoplay: { // 자동 재생 여부
